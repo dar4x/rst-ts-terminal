be87ad3e74e9902d343d2762a0a1f9b65b744e40
Islam <islamxdaamduu@gmail.com>
2023-07-28 22:44:05 +0600

    little bit changed ticket component

--- src/components/TicketComponent.tsx
+++ src/components/TicketComponent.tsx
@@ -2,19 +2,45 @@ import React, { useEffect, useState } from 'react';
 import LogoSVG from '../assets/images/RSK_Bank_Logo1.svg';
 import styles from './TicketComponent.module.scss';
 import QRCode from 'qrcode.react';
+import { getIDFunc, getSavedCategory, getSavedQUEUE } from 'src/functions/SaveIDFunc';
+import $axios from 'src/utils/axios';
+import { BASE_URL } from 'src/utils/const';
 
 
 const TicketComponent = ({ id }: { id: string }) => {
   const [qrReady, setQrReady] = useState(false);
 
+  const savedCategory = getSavedCategory();
+  const savedData = getSavedQUEUE();
+  const savedServices = getIDFunc();
+
+  const talonDATA: any = localStorage.getItem("printTALON");
+  const modifiedTalonDATA = JSON.parse(talonDATA);
+
+  let talonForPrinting: any[] = [];
+
+  const getPrintingTalonDATA = async () => {
+    try {
+      const response = await $axios.get(`${BASE_URL}/customers/printing/${modifiedTalonDATA.customer_id}`);
+      talonForPrinting = response.data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  console.log(talonForPrinting)
+
   useEffect(() => {
     setQrReady(true);
+    getPrintingTalonDATA()
   }, []);
 
   return (
     <div id={id} className={styles.ticket_header}>
+      <div className={styles.logo}>
+        <img src={LogoSVG} alt="" />
+      </div>
       <div className={styles.ticket_body}>
-        <div className={styles.title}>Б201</div>
+        <div className={styles.title}></div>
         <div className={styles.time}>Время выдачи:10:30</div>
         <div className={styles.detail}>Юридическое лицо/<br />Открытие счета</div>
         <div className={styles.window}>Окно:3</div>
--- src/pages/VerificationPage.tsx
+++ src/pages/VerificationPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Verification.module.scss';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { getIDFunc, getSavedCategory, getSavedQUEUE } from 'src/functions/SaveIDFunc';
 import { fetchCustomersAdd } from 'src/store/features/branchesSlice';
 import TicketComponent from 'src/components/TicketComponent';
+import { useReactToPrint } from 'react-to-print';
 
 const VerificationPage = () => {
   const handleClick = (): void => {
@@ -16,26 +17,14 @@ const VerificationPage = () => {
   const savedData = getSavedQUEUE();
   const savedServices = getIDFunc();
 
-  const handlePrint = () => {
-    const printWindow = window.open('', '_blank');
-    if (printWindow) {
-      printWindow.document.write('<html><head><title>Ticket</title></head><body>'); // Открываем HTML документ в новом окне для печати
-      printWindow.document.write('<div style="margin: 20px;">');
-      printWindow.document.write('<h1>Талон</h1>');
-      printWindow.document.write('<hr />');
-      printWindow.document.write('<div>');
-      const ticketContent = document.getElementById(savedData.id); // Получаем содержимое талона по переданному ID
-      if (ticketContent) {
-        printWindow.document.write(ticketContent.innerHTML); // Вставляем содержимое талона в новое окно для печати
-      }
-      printWindow.document.write('</div>');
-      printWindow.document.write('</div>');
-      printWindow.document.write('</body></html>');
-      setTimeout(() => {
-        printWindow.print(); // Вызываем печать после задержки
-        printWindow.document.close(); // Закрываем документ после печати
-      }, 500); // Задержка в 500 миллисекунд (может потребоваться регулировка)
-    }
+  const componentRef = React.useRef(null); // Создаем ref для компонента TicketComponent
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current, // Указываем элемент для печати
+  });
+
+  const handlePrintClick = () => {
+    handlePrint(); // Вызываем функцию печати из хука useReactToPrint
     fetchCustomersAdd(savedCategory, savedData.id);
   }
 
@@ -48,15 +37,15 @@ const VerificationPage = () => {
       </div>
       <div className={styles.header__content}>
         <p>{ savedServices.name === "payment cards" ? t("paymentCards") : savedServices.name === "legal entities" ? t("legalEntity") : savedServices.name === "individual" ? t("physicalEntity") : ""}/{ savedData.name }</p>
-        <p>{t("special")}/{ savedCategory === "pensioner" ? t("privileges3") : savedCategory === "pregnant" ? t("privileges2") : savedCategory === "disabled person" ? t("privileges1") : ""}</p>
-
+        <p>{t("special")}/{ savedCategory === "pensioner" ? t("privileges3") : savedCategory === "pregnant" ? t("privileges2") : savedCategory === "disabled person" ? t("privileges1") : "Нет"}</p>
+        <div className={styles.white_block}></div>
         <div className={styles.btns}>
-          <button className={styles.btn} onClick={handlePrint}>
+          <button className={styles.btn} onClick={handlePrintClick}>
             { t("talon") }
           </button>
-          <div className={styles.talon} >
-            <TicketComponent id={savedData.id} />
-          </div>
+           <div className={styles.talon} ref={componentRef}>
+             <TicketComponent id={savedData.id} />
+           </div>
           <Link to={'#'} onClick={handleClick} className={styles.btn}>
             { t("back") }
           </Link>
